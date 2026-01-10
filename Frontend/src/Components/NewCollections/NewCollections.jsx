4c40bfea1e7a041ee36492c70333697a823d45bb
Abhishek DungDung <abhishekdungdung140703@gmail.com>
2026-01-11 02:19:30 +0530

    frontend ready

--- Frontend/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/src/Components/NewCollections/NewCollections.jsx
@@ -3,13 +3,13 @@ import "./NewCollection.css";
 import Item from "../Item/Item";
 
 const NewCollections = () => {
-  const [new_collection, setNew_Collection] = useState([]);
+  const [new_collection, setNewCollection] = useState([]);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/newcollections`)
-      .then((response) => response.json())
-      .then((data) => setNew_Collection(data))
-      .catch((err) => console.error("New Collections Error:", err));
+      .then((res) => res.json())
+      .then((data) => setNewCollection(data))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
@@ -17,18 +17,16 @@ const NewCollections = () => {
       <h1>NEW COLLECTIONS</h1>
       <hr />
       <div className="collections">
-        {new_collection.map((item, i) => {
-          return (
-            <Item
-              key={i}
-              id={item.id}
-              name={item.name}
-              image={item.image}
-              neww_price={item.new_price}
-              old_price={item.old_price}
-            />
-          );
-        })}
+        {new_collection.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   );
